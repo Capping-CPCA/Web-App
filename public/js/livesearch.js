a3f20d290af1cb456b722bbf12b650df42d5a270
vmjoseph <valliejoseph@gmail.com>
2017-10-11 10:42:10 -0400

    live search currenty working, going to make it into a class for easier usability
    live search now submits form on auto-complete click, need to test what happens when multiple participants have the same name
    
    adjusting auto complete function
    
    changing name of branch to match branch naming scheme
    
    adding global function for live searching
    
    leaving in db init file for now, fixing live search
    
    Fixing lag issue that occurs during search
    
    fixed submit bug on live search
    
    finished live search feature, updated so that function is globally accessible
    
    adding jsdoc comments and comment headers, removing debugging
    
    simplifying filtered function
    
    removing test file
    
    updating references to form in livesearch function
    
    removing reference to test search file
    
    fixed issue with force list element option
    
    fixing version number
    Fixes #15
    
    Added live search both to Agent Requests and for global use
    
    added ui hover selection for search results
    
    removed live search test

---
+++ public/js/livesearch.js
@@ -0,0 +1,116 @@
+/**
+ * PEP Capping 2017 Algozzine's Class
+ *
+ * Live search js function that allows users to search while typing and view results on their current page
+ *
+ * Ussage: When there exists a page where a user has the ability to seach/query a database, this class will
+ * take in user specified input and query the database with each keystroke. The results are sent back to the
+ * origin page (where the user is), and displayed in a parent/child based view.
+ *
+ * @author Vallie Joseph
+ * @copyright 2017 Marist College
+ * @since 0.1.2
+ * @version 0.1.2
+ *
+ * @param input - form input given by the user
+ * @param results - the main section in which results will be displayed, must of parent/child structure
+ * i.e Unordered Lists, Ordered Lists, or Tables
+ * @param urlResult - where the ajax url will point to- see AJAX url documentation
+ * @param formMethod - method in which ajax will submit the form - see AJAX  type documentation
+ * @param typeData - type that ajax will return your data in, options are text, html, and json
+ * @param resultFilter - this is how you will extract the specific desired elements from your results, for example,
+ * If the user types 'John Doe' into the form input field, AJAX will return the information it recieves from a succesful submission.
+ * If the submission is succesful, the returned information may be html (based on developer specifications in typeData)
+ * If we return html, we may only be interested in the list elements of the results- this is the place to specify that
+ * @setInputListener = constructor
+ */
+
+//init vars
+var input;
+var results;
+var indiResults;
+var urlResult;
+var formMethod;
+var typeData;
+var resultFilter;
+
+//set values to vars, this is done wherever the livesearch is called
+function setInputListener(inputField , resultField, resultHolder, uUrl, method, typeD){
+
+	input = inputField;
+	results = resultField;
+	indiResults = resultHolder;
+	urlResult =  uUrl + '/';
+	typeData = typeD;
+	return this;
+}
+
+//set the method type for ajax submission
+function setMethod(method){
+	var regex= /(GET|PUT)/g;
+	var methods = method.match(regex);
+	if(methods != null){
+		return this;
+	}
+	console.log("The method was not set correctly");
+	return false;
+}
+
+//setting the filter / find for returning only desired result html
+function setResultFilter(filter){
+	resultFilter = filter;
+	return this;
+}
+
+//actual jquery live search fn, takes in the form where live search is being performed
+(function( $form ){
+	$.fn.liveSearch = function() {
+		//giving the form it's own var for simplicity sake, later functions redefine the 'this' method
+		var uForm = $(this);
+
+		//taking cleaning user supplied input - no extra spaces or , will be queried
+		function filterResults(raw){
+			return raw.replace(/\s+/g,' ').trim().replace(',','');;
+		}
+		//set the form input element text to cleaned text, nice and tidy
+		uForm.find(":text").val(filterResults(uForm.find(":text").first().val()));
+		var userInput=  uForm.find(":text").val();
+
+		//ajax - asynchronously requesting resutls from our result page
+		$.ajax({
+			type: formMethod,
+			url: urlResult+userInput,
+			dataType: typeData,
+			data: uForm.serialize(),
+			success:function(data){
+				//clear the search result display each time we look up new results - otherwise duplicates show
+				results.empty();
+				var userResults = $(data).find(resultFilter);
+
+				//jquery for each short hand, populating result list with well...results
+				$(userResults).each(function(){
+					//creates new children for dev specified parent
+					$(results).append($(indiResults).addClass('search-result').html($(this).text()));
+
+				});
+				//whenever a result is clicked, complete the search to direct the user to the results page
+				//this should work with any child of the desired element (Ex: rows in a table, list elemetns in a list)
+                $(results).children().each(function() {
+                    $(this).click(function(){
+                        //first make sure the user input is updated
+                        input.val(filterResults($(this).text()));
+                        //submit that form!
+                        uForm.submit();
+                    });
+
+                    $(this).keypress(function(e) {
+                        if (e.key === 'Enter' || e.key === 'Space') {
+                            $(this).click();
+                        }
+                    });
+                });
+			}
+		});
+		return this;
+	};
+})( jQuery );
--- views/agency-requests/agency_requests.php
+++ views/agency-requests/agency_requests.php
@@ -1,4 +1,18 @@
 <?php
+/**
+ * PEP Capping 2017 Algozzine's Class
+ *
+ * Allows a user to search for participants in the database.
+ *
+ * The main purpose of this page is to allow users a quick way to
+ * access participant information (specifically agency requests). The
+ * page uses a live search that
+ *
+ * @author Vallie Joseph
+ * @copyright 2017 Marist College
+ * @since 0.1.2
+ * @version 0.1.2
+ */
 authorizedPage();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -16,13 +30,34 @@ include('header.php');
             <br />
             <form class="search-agency" method="POST" action="/agency-requests">
                 <div class="form-group">
-                    <input type="text" class="form-control" name="searchquery" placeholder="Begin typing participant's name...">
+                    <input type="text" class="form-control user-search" name="searchquery" placeholder="Begin typing participant's name...">
+                    <ul class='list-group'></ul>
                 </div>
-                <button type="submit" class="btn cpca form-control">Submit</button>
+                <button type="submit" class="btn cpca form-control submit-search">Submit</button>
             </form>
         </div>
     </div>
 
+    <script>
+        //Disables the default browser autocomplete
+        $( ".search-agency" ).attr( "autocomplete", "off" );
+
+        //construct live search, needs the user input field, where search results will go,
+        //desired class name of search result elements, url for where ajax is sending the request, and
+        //type of data ajax is returning
+        setInputListener($(".user-search"),$(".list-group"),"<li class='list-group-item suggestion' tabindex='0'></li>", "/participant-search", "html");
+
+        //Set the ajax submit method to either put or get, will default to GET if left blank
+        setMethod("PUT");
+
+        //set what class you're lookin for in the search results
+        setResultFilter('.list-group-item span');
+
+        //search dynamically with every user input
+        $(".user-search").keyup(function(){
+            $('.search-agency').liveSearch();
+        });
+    </script>
 
 <?php
 include('footer.php');
