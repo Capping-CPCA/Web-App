6bd0d6108d42347dfac063e1c98c80bf4f57fb87
john.grzechowiak1 <john.grzechowiak1@marist.edu>
2017-10-17 09:54:38 -0400

    Version tag
    - Displays current version in footer
    
    Ref #37

--- public/footer.php
+++ public/footer.php
@@ -1,8 +1,27 @@
+<?php
+/**
+ * PEP Capping 2017 Algozzine's Class
+ *
+ * Footer for the main HTML webpage.
+ *
+ * This file should be paired with the header.php file
+ * to complete the basic framework for the HTML page.
+ * This specific file contains the footer information
+ * displayed on the page such as the copyright and the
+ * version number.
+ *
+ * @author Jack Grzechowiak
+ * @copyright 2017 Marist College
+ * @version 0.1.3
+ * @since 0.1.3
+ */
+?>
             </div> <!-- "#main-content" div -->
         </div>
         <nav class="navbar navbar-dark bg-dark">
             <footer>
                 Copyright &copy; 2017 Marist College
+                <span class="float-right"><?= VERSION ?></span>
             </footer>
         </nav>
     </div> <!-- ".non-menu-content" div -->
