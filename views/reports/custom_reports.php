0d70241e0e67396e1f714a6960f0773cdbd0b7af
stickzman <Daniel.ahl1@marist.edu>
2017-10-25 20:27:02 -0400

    Changed db to point at real db
    Fixed date bug
    
    Fixes #60, adds complete functionality to report pages, printing CSS, and points at the live database server
    
    Fixed version comments, include('header') position, and removed authorizedPage()
    
    Replaced month switch-case with echo statement
    
    Changed 'Run Another Report' button onlick to window.history.back(), but the values are not refilled..
    
    Report tables now hidden until first report is generated
    
    Made displaying all/only the main-content div when printing the default behavior for the whole project
    
    Replaced indv queries with calls to new queryBuilder() function
    
    Fixed custom.css issue

--- views/reports/custom_reports.php
+++ views/reports/custom_reports.php
@@ -1,177 +1,220 @@
 <?php
-/**
- * PEP Capping 2017 Algozzine's Class
- *
- * Allows users to create a custom report.
- *
- * This page has a form that allows users to specify what
- * information they would like to see in a custom report.
- * The 'Generate Report' button will kick off a custom query
- * to the database and display the results in a new page
- * (defined in 'custom_reports_table.php')
- *
- * @author Rafael Mormol & Daniel Ahl
- * @copyright 2017 Marist College
- * @version 0.1.5
- * @since 0.1.4
- */
-    global $params, $route, $view;
-    include('header.php');
-    ?>
-<div class="container" >
-    <h3 align="center" style="margin-bottom: 5%">Custom Report</h3>
-    <form style="margin-left: 30%">
-        <fieldset>
-            <!-- Select Basic -->
-            <div class="form-group row">
-                <label class="col-md-2 col-form-label" for="month"><b>Month</b></label>
-                <div class="col-md-4">
-                    <select id="month" name="month" class="form-control">
-                        <option value="January">January</option>
-                        <option value="February">February</option>
-                        <option value="March">March</option>
-                        <option value="April">April</option>
-                        <option value="May">May</option>
-                        <option value="June">June</option>
-                        <option value="July">July</option>
-                        <option value="August">August</option>
-                        <option value="September">September</option>
-                        <option value="October">October</option>
-                        <option value="November">November</option>
-                        <option value="December">December</option>
-                    </select>
-                </div>
-            </div>
-            <!-- Select Basic -->
-            <div class="form-group row">
-                <label class="col-md-2 col-form-label" for="year"><b>Year</b></label>
-                <div class="col-md-4">
-                    <select id="year" name="year" class="form-control">
-                        <option value="January">2017</option>
-                        <option value="February">2016</option>
-                        <option value="March">2015</option>
-                        <option value="April">2014</option>
-                        <option value="May">2013</option>
-                        <option value="June">2012</option>
-                        <option value="July">2011</option>
-                    </select>
-                </div>
-            </div>
-            <!-- Multiple Checkboxes -->
-            <div class="form-group row">
-                <label class="col-md-2 col-form-label" for="location"><b>Location</b></label>
-                <div class="col-md-4">
-                    <div class="checkbox">
-                        <label for="location-0">
-                        <input type="checkbox" name="location" id="location-0" value="Cornerstone">
-                        Cornerstone
-                        </label>
-                    </div>
-                    <div class="checkbox">
-                        <label for="location-1">
-                        <input type="checkbox" name="location" id="location-1" value="Dutches County Jail">
-                        Dutches County Jail
-                        </label>
-                    </div>
-                    <div class="checkbox">
-                        <label for="location-2">
-                        <input type="checkbox" name="location" id="location-2" value="Florence Manor">
-                        Florence Manor
-                        </label>
-                    </div>
-                    <div class="checkbox">
-                        <label for="location-3">
-                        <input type="checkbox" name="location" id="location-3" value="Fox Run">
-                        Fox Run
-                        </label>
-                    </div>
-                    <div class="checkbox">
-                        <label for="location-4">
-                        <input type="checkbox" name="location" id="location-4" value="ITAP Meadow Run">
-                        ITAP Meadow Run
-                        </label>
-                    </div>
-                </div>
-            </div>
-            <!-- Multiple Checkboxes -->
-            <div class="form-group row">
-                <label class="col-md-2 col-form-label" for="race"><b>Race</b></label>
-                <div class="col-md-4">
-                    <div class="checkbox">
-                        <label for="race-0">
-                        <input type="checkbox" name="race" id="race-0" value="Caucasian">
-                        Caucasian
-                        </label>
-                    </div>
-                    <div class="checkbox">
-                        <label for="race-1">
-                        <input type="checkbox" name="race" id="race-1" value="African American">
-                        African American
-                        </label>
-                    </div>
-                    <div class="checkbox">
-                        <label for="race-2">
-                        <input type="checkbox" name="race" id="race-2" value="Multi Racial">
-                        Multi Racial
-                        </label>
-                    </div>
-                    <div class="checkbox">
-                        <label for="race-3">
-                        <input type="checkbox" name="race" id="race-3" value="Latino">
-                        Latino
-                        </label>
-                    </div>
-                    <div class="checkbox">
-                        <label for="race-4">
-                        <input type="checkbox" name="race" id="race-4" value="Pacific Islander">
-                        Pacific Islander
-                        </label>
-                    </div>
-                    <div class="checkbox">
-                        <label for="race-5">
-                        <input type="checkbox" name="race" id="race-5" value="Native American">
-                        Native American
-                        </label>
-                    </div>
-                    <div class="checkbox">
-                        <label for="race-6">
-                        <input type="checkbox" name="race" id="race-6" value="Other">
-                        Other
-                        </label>
-                    </div>
-                </div>
-            </div>
-            <!-- Multiple Checkboxes -->
-            <div class="form-group row">
-                <label class="col-md-2 col-form-label" for="age"><b>Age</b></label>
-                <div class="col-md-4">
-                    <div class="checkbox">
-                        <label for="age-0">
-                        <input type="checkbox" name="age" id="age-0" value="January">
-                        20-40
-                        </label>
-                    </div>
-                    <div class="checkbox">
-                        <label for="age-1">
-                        <input type="checkbox" name="age" id="age-1" value="February">
-                        41-64
-                        </label>
-                    </div>
-                    <div class="checkbox">
-                        <label for="age-2">
-                        <input type="checkbox" name="age" id="age-2" value="March">
-                        65+
-                        </label>
-                    </div>
-                </div>
-            </div>
-            <!-- Button -->
-            <div class="form-group">
-                <div class="col-md-4">
-                    <a href="custom-reports-table" class="btn cpca">Generate Report</a>
-                </div>
-            </div>
-        </fieldset>
-    </form>
+	/**
+	 * PEP Capping 2017 Algozzine's Class
+	 *
+	 * Allows users to create a custom report.
+	 *
+	 * This allows users to specify what information they would 
+	 * like to see in a custom report.
+	 * The 'Generate Report' button will kick off a custom query
+	 * to the database and display the results in a new page
+	 * (defined in 'custom_reports_table.php')
+	 *
+	 * @author Daniel Ahl
+	 *		   Rafael Mormol
+	 * @copyright 2017 Marist College
+	 * @version 0.3
+	 * @since 0.1.4
+	 */		
+	
+	include('header.php');
+?>
+<div class="container py-5">
+	<div class="col-auto" >
+		<form action="custom-reports-table" method="POST" autocomplete="on">
+			<fieldset>
+				<!-- Select Basic -->
+				<div class="form-group row">
+					<label class="col-md-2 col-form-label" for="month"><b>Month</b></label>
+					<div class="col-md-4">
+						<select id="month" name="month" class="form-control">
+							<option value="1">January</option>
+							<option value="2">February</option>
+							<option value="3">March</option>
+							<option value="4">April</option>
+							<option value="5">May</option>
+							<option value="6">June</option>
+							<option value="7">July</option>
+							<option value="8">August</option>
+							<option value="9">September</option>
+							<option value="10">October</option>
+							<option value="11">November</option>
+							<option value="12">December</option>
+						</select>
+					</div>
+				</div>
+				<!-- Select Basic -->
+				<div class="form-group row">
+					<label class="col-md-2 col-form-label" for="year"><b>Year</b></label>
+					<div class="col-md-4">
+						<select id="year" name="year" class="form-control">
+							<!-- YEAR OPTIONS INIT IN JAVASCRIPT BELOW.
+								BASED ON CURRENT YEAR.-->
+						</select>
+					</div>
+				</div>
+				<!-- Multiple Checkboxes -->
+				<div class="form-group row">
+					<label class="col-md-2 col-form-label" for="location[]"><b>Location</b></label>
+					<div class="col-md-4">
+						<div class="checkbox">
+							<label for="location-0">
+							<input type="checkbox" name="location[]" id="location-0" value="Cornerstone">
+							Cornerstone
+							</label>
+						</div>
+						<div class="checkbox">
+							<label for="location-1">
+							<input type="checkbox" name="location[]" id="location-1" value="Dutchess County Jail">
+							Dutchess County Jail
+							</label>
+						</div>
+						<div class="checkbox">
+							<label for="location-2">
+							<input type="checkbox" name="location[]" id="location-2" value="Florence Manor">
+							Florence Manor
+							</label>
+						</div>
+						<div class="checkbox">
+							<label for="location-3">
+							<input type="checkbox" name="location[]" id="location-3" value="Fox Run">
+							Fox Run
+							</label>
+						</div>
+						<div class="checkbox">
+							<label for="location-4">
+							<input type="checkbox" name="location[]" id="location-4" value="ITAP Meadow Run">
+							ITAP Meadow Run
+							</label>
+						</div>
+					</div>
+				</div>
+				<!-- Multiple Checkboxes -->
+				<div class="form-group row">
+					<label class="col-md-2 col-form-label" for="race[]"><b>Race</b></label>
+					<div class="col-md-4">
+						<div class="checkbox">
+							<label for="race-0">
+							<input type="checkbox" name="race[]" id="race-0" value="Caucasian">
+							Caucasian
+							</label>
+						</div>
+						<div class="checkbox">
+							<label for="race-1">
+							<input type="checkbox" name="race[]" id="race-1" value="African American">
+							African American
+							</label>
+						</div>
+						<div class="checkbox">
+							<label for="race-2">
+							<input type="checkbox" name="race[]" id="race-2" value="Multi Racial">
+							Multi Racial
+							</label>
+						</div>
+						<div class="checkbox">
+							<label for="race-3">
+							<input type="checkbox" name="race[]" id="race-3" value="Latino">
+							Latino
+							</label>
+						</div>
+						<div class="checkbox">
+							<label for="race-4">
+							<input type="checkbox" name="race[]" id="race-4" value="Pacific Islander">
+							Pacific Islander
+							</label>
+						</div>
+						<div class="checkbox">
+							<label for="race-5">
+							<input type="checkbox" name="race[]" id="race-5" value="Native American">
+							Native American
+							</label>
+						</div>
+						<div class="checkbox">
+							<label for="race-6">
+							<input type="checkbox" name="race[]" id="race-6" value="Other">
+							Other
+							</label>
+						</div>
+					</div>
+				</div>
+				<!-- Multiple Checkboxes -->
+				<div class="form-group row">
+					<label class="col-md-2 col-form-label"><b>Age</b></label>
+					<div class="col-md-5">
+						<div class="row">
+							<div class="col-md-5">
+								<select id="minAge" name="minAge" class="form-control" onchange="minAgeChange()">
+									<option value="any">Any</option>
+									<?php
+										for ($i = 65; $i >= 18; $i--) {
+											echo "<option value='$i'>$i</option>";
+										}
+										?>
+								</select>
+							</div>
+							<div class="col-md-1" align="center">To</div>
+							<div class="col-md-5">
+								<select id="maxAge" name="maxAge" class="form-control" onchange="maxAgeChange()">
+									<option value="any">Any</option>
+									<?php
+										for ($i = 65; $i >= 18; $i--) {
+											echo "<option value='$i'>$i</option>";
+										}
+										?>
+								</select>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- Submit -->
+				<div class="form-group pt-2">
+					<div class="col-md-7" align="center">
+						<button type="submit" class="btn cpca">Generate Report</button>
+					</div>
+				</div>
+			</fieldset>
+		</form>
+	</div>
 </div>
+<script>
+	const NUM_YEARS_BACK = 4;
+	
+	window.onload = initPage;
+	
+	function initPage() {
+		var d = new Date();
+		var monthElem = document.getElementById("month");
+		monthElem.selectedIndex = d.getMonth();
+		var yearElem = document.getElementById("year");
+		var year = d.getFullYear();
+		for (i = 0; i <= NUM_YEARS_BACK; i++) {
+			var opt = document.createElement('option');
+			opt.value = year - i;
+			opt.innerHTML = year - i;
+			yearElem.appendChild(opt);
+		}
+	}
+	
+	function minAgeChange() {
+		var minIndex = document.getElementById("minAge").selectedIndex;
+		var maxList = document.getElementById("maxAge").options;
+		if (minIndex === 0) {
+			for (i = 1; i < maxList.length; i++) {
+				maxList[i].disabled = false;
+			}
+		} else {
+			for (i = 1; i < maxList.length; i++) {
+				maxList[i].disabled = i > minIndex;
+			}
+		}
+	}
+	
+	function maxAgeChange() {
+		var maxIndex = document.getElementById("maxAge").selectedIndex;
+		var minList = document.getElementById("minAge").options;
+		for (i = 1; i < minList.length; i++) {
+			minList[i].disabled = i < maxIndex;
+		}
+	}
+</script>
 <?php include('footer.php'); ?>
--- views/reports/custom_reports_table.php
+++ views/reports/custom_reports_table.php
@@ -1,60 +1,172 @@
 <?php
-/**
- * PEP Capping 2017 Algozzine's Class
- *
- * Displays the results of a custom report.
- *
- * This page will display the information requested in the form
- * on custom_reports.php page, formatted into a single table.
- *
- * @author Rafael Mormol & Daniel Ahl
- * @copyright 2017 Marist College
- * @version 0.1.5
- * @since 0.1.4
- */
-    global $params, $route, $view;
-    include('header.php');
-    ?>
+	/**
+	 * PEP Capping 2017 Algozzine's Class
+	 *
+	 * Displays the results of a custom report.
+	 *
+	 * This page display the information requested in the form
+	 * on custom_reports.php page, formatted into a single table.
+	 *
+	 * @author Daniel Ahl
+	 *		   Rafael Mormol
+	 * @copyright 2017 Marist College
+	 * @version 0.3
+	 * @since 0.1.4
+	 */
+	if ($_SERVER['REQUEST_METHOD'] !== 'POST') header('Location: custom-reports');
+	
+	global $db;
+
+	$month = $_POST['month'];
+	$year = $_POST['year'];
+	$locs = isset($_POST['location']) ? $_POST['location'] : [];
+	$races = isset($_POST['race']) ? $_POST['race'] : [];
+	$minAge = $_POST['minAge'];
+	$maxAge = $_POST['maxAge'];
+	$monthQuery = "(date_part('month', participantclassattendance.date) = $month)";
+	$yearQuery = "(date_part('year', participantclassattendance.date) = $year)";
+	$locQuery = "";
+	
+	if (count($locs) > 0) {
+		$locQuery = "(participantclassattendance.siteName = '" . $locs[0] . "' ";
+		for ($i = 1; $i < count($locs); $i++) {
+			$locQuery .= "OR participantclassattendance.siteName = '" . $locs[$i] . "' ";
+		}
+		$locQuery .= ")";
+	}
+	
+	$raceQuery = "";
+	if (count($races) > 0) {
+		$raceQuery = "(participants.race = '" . $races[0] . "' ";
+		for ($i = 1; $i < count($races); $i++) {
+			$raceQuery .= "OR participants.race = '" . $races[$i] . "' ";
+		}
+		$raceQuery .= ")";
+	}
+	$ageQuery = "";
+	if ($minAge !== 'any') {
+		$ageQuery = "((date_part('year', AGE(participants.dateOfBirth)) >= $minAge) ";
+	}
+	if ($maxAge !== 'any') {
+		if ($minAge === 'any') {
+			$ageQuery = "(";
+		} else {
+			$ageQuery .= "AND ";
+		}
+		$ageQuery .= "(date_part('year', AGE(participants.dateOfBirth)) <= $maxAge))";
+	} else {
+		if ($ageQuery !== "") $ageQuery .= ")";
+	}
+	
+	$yearWhereClause = "$yearQuery ";
+	if ($locQuery !== "") $yearWhereClause .= "AND $locQuery ";
+	if ($raceQuery !== "") $yearWhereClause .= "AND $raceQuery ";
+	if ($ageQuery !== "") $yearWhereClause .= "AND $ageQuery ";
+	
+	$monthWhereClause = $yearWhereClause . "AND $monthQuery ";
+	$newWhereClause = $monthWhereClause . "AND participantclassattendance.isnew = TRUE;";
+	$monthWhereClause .= ";";
+	
+	$baseQuery = "SELECT COUNT(DISTINCT(participants.participantid)) as Participants
+				FROM participants INNER JOIN participantclassattendance
+				ON participants.participantid = participantclassattendance.participantid
+				WHERE ";
+				
+	$monthRes = pg_fetch_result($db->query($baseQuery . $monthWhereClause, []), 0, 0);
+	$newRes = pg_fetch_result($db->query($baseQuery . $newWhereClause, []), 0, 0);
+	$duplRes = $monthRes - $newRes;
+	$yearRes = pg_fetch_result($db->query($baseQuery . $yearWhereClause, []), 0, 0);
+	
+	include('header.php');
+?>
 <div class="container">
-    <h3 align="center">Custom Report</h3>
-    <br />
-    <h6 align="center">Month: October</h6>
-    <h6 align="center">Year: 2016</h6>
-    <br />
-    <h6 align="center">Location: Cornerstone | Race: African American | Age: 41-64 </h6>
-    <table class="table table-hover table-striped table-bordered">
-        <thead>
-            <tr>
-                <th>
-                    Current Month
-                </th>
-                <th>
-                    Newly Served
-                </th>
-                <th>
-                    Duplicate Served
-                </th>
-                <th>
-                    YTD
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>
-                    7
-                </td>
-                <td>
-                    2
-                </td>
-                <td>
-                    0
-                </td>
-                <td>
-                    24
-                </td>
-            </tr>
-        </tbody>
-    </table>
+	<div class="container py-2">
+		<div align="center">
+			<h2><?=cal_info(0)['months'][$month] . " " . $year;?></h2>
+		</div>
+		<div align="center">
+			<?php
+			#Display the chosen locations
+			if (count($locs) > 0) {
+				echo "<div><b>Locations:</b> " . $locs[0];
+				for ($i = 1; $i < count($locs); $i++) {
+					echo ", " . $locs[$i];
+				}
+				echo "</div>";
+			}
+			
+			#Display the chosen races
+			if (count($races) > 0) {
+				echo "<div><b>Races:</b> " . $races[0];
+				for ($i = 1; $i < count($races); $i++) {
+					echo ", " . $races[$i];
+				}
+				echo "</div>";
+			}
+			
+			#Display the chosen ages
+			if ($minAge !== 'any' || $maxAge !=='any') {
+				if ($minAge === $maxAge) {
+					echo "<div><b>Age Range:</b> " . $maxAge . "</div>";
+				} elseif ($minAge === 'any') {
+					echo "<div><b>Age Range:</b> " . $maxAge . " and below</div>";
+				} elseif ($maxAge === 'any') {
+					echo "<div><b>Age Range:</b> " . $minAge . " and above</div>";
+				} else {
+					echo "<div><b>Age Range:</b> " . $minAge . " - " . $maxAge . "</div>";
+				}
+			}
+			?>
+		</div>
+	</div>
+	<div class="container py-2">
+		<table class="table table-hover table-striped table-bordered">
+			<thead>
+				<tr>
+					<th>
+						Current Month
+					</th>
+					<th>
+						Newly Served
+					</th>
+					<th>
+						Duplicate Served
+					</th>
+					<th>
+						Year
+					</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>
+						<?=$monthRes?>
+					</td>
+					<td>
+						<?=$newRes?>
+					</td>
+					<td>
+						<?=$duplRes?>
+					</td>
+					<td>
+						<?=$yearRes?>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	<div class="container" align="center">
+		<button class="btn cpca" onclick="window.history.back();">Run Another Report</button>
+	</div>
 </div>
+<style>
+@media print{
+	.btn {
+		display: none;
+	}
+	.form-group {
+		display: none;
+	}
+}
+</style>
 <?php include('footer.php'); ?>
